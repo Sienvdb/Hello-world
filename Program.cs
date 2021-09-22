0a9a4b811a74839b777d2fa0a61618ad46c68370
Sien Van den Bergh <sienvdbergh@gmail.com>
2021-09-22 15:36:48 +0200

    mijn eerste git

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace achitecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
